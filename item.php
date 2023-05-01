7929fb7a816594d4dcdde3b2096d724640c14c2c
SqUaRe <33025882+SamuelWM@users.noreply.github.com>
2023-04-30 22:39:16 -0400

    Uploaded source for LAMP item insert.
    Set of files used to create the item insert functionality. While it isn't the final version of this feature, it is capable of inserting entries into the MySQL table.

---
+++ item.php
@@ -0,0 +1,23 @@
+<?php
+// connect to sql db 
+// $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
+
+$con = mysqli_connect('localhost', 'root', 'IS436','pos');
+
+// get the post records
+$tiid = $_POST['tiid'];
+$tiname = $_POST['tiname'];
+$tidesc = $_POST['tidesc'];
+
+// database insert SQL code
+$sql = "INSERT INTO `items` (`iid`, `iname`, `idesc`) VALUES ('$tiid', '$tiname', '$tidesc')";
+
+// insert in database 
+$rs = mysqli_query($con, $sql);
+
+if($rs)
+{
+	echo "Contact Records Inserted";
+}
+
+?>
---
+++ item_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE items (
+    iid int,
+    iname varchar(255),
+    idesc varchar(255),
+    PRIMARY KEY (iid)
+);
+
+insert into items (iid, iname, idesc) VALUES ('0', 'testing', 'testing 123');
